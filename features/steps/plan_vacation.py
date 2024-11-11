44a94e113fbdc587c735fce902267791539f429d
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2024-11-11 13:14:46 -0700

    Delete extra files

--- features/steps/plan_vacation.py
+++
@@ -1,6 +0,0 @@
-from behave import *
-
-@when(u'I enter a departure date as "{departure_date}"')
-def step_impl(context, departure_date):
-    context.main_page.enter_departure_date(departure_date)
-    context.departure_date = departure_date
